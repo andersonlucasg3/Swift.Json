02a129aea76e2a7f45c998f18580b5ee14fbc949
MrJorgeXavier <mr.jorge.xavier@gmail.com>
2017-12-25 21:39:22 -0200

    implementando configuração de tradução entre as notações de codificação json-objeto

--- Sources/JsonConfig.swift
+++ Sources/JsonConfig.swift
@@ -8,6 +8,38 @@
 
 import Foundation
 
+public enum ParseRule {
+    case camelCase
+    case snakeCase
+    case hyphenCase
+    case pascalCase
+    case any
+    
+    fileprivate func stringIs(_ rule: ParseRule, _ string: String) -> Bool {
+        switch rule {
+        case .camelCase: return !string.contains("-") && !string.contains("_") && string.first == string.lowercased().first
+        case .pascalCase: return !string.contains("-") && !string.contains("_") && string.first == string.uppercased().first
+        case .snakeCase: return !string.contains("-") && string == string.lowercased()
+        case .hyphenCase: return !string.contains("_") && string == string.lowercased()
+        case .any: return true
+        }
+    }
+}
+
+public typealias ParseRules = (jsonRule: ParseRule, objectRule: ParseRule)
+
+public struct ParseOptions {
+    let rules: ParseRules
+    
+    ///default rules configuration: json file is interpreted as if snake_case, and the NSObject is interpreted as camelCase.
+    public init(_ rules: ParseRules = (jsonRule: .snakeCase, objectRule: .camelCase)) {
+        self.rules = rules
+    }
+    
+    
+}
+
+
 /// JsonConvertBlock is the block to convert values from object to json and json to object.
 public typealias JsonConvertBlock = ((_ object: AnyObject, _ andKey: String) -> AnyObject?)
 
